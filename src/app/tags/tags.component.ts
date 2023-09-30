347ac5fcb10c03435bfdd02c1b2d8b47c5fad782
Emi1Mel0 <emilwagdy11@gmail.com>
2023-09-30 04:44:46 +0300

    creating tags array, starting page by it, style

--- src/app/tags/tags.component.ts
+++ src/app/tags/tags.component.ts
@@ -1,7 +1,6 @@
-
 import { FoodService } from '../services/food/food.service';
 import { tag } from './../shared/models/tag';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tags',
@@ -10,8 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TagsComponent implements OnInit {
   constructor(private _FoodService: FoodService) {}
-  tags: tag[] = [];
+  @Input()
+  foodDetailsTags?: string[];
+  // and we don't need numbers
+  // we just need the tag
+
+@Input()
+justifyContent:string = 'center';
+// because it set as it in the css file by default but we need to change it at the details page
+
+
+  tags?: tag[];
+  // it was just array but we change it because
+  // we will not always use it as its builds so sometimes shown
+  // at details page we just need the tags of its specific food
+  // not all foods
 
   ngOnInit(): void {
-this.tags = this._FoodService.getAllTags();  }
+    if (!this.foodDetailsTags) {
+      this.tags = this._FoodService.getAllTags();
+    }
+  }
 }
