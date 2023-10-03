b8d43a3b4c298b244745948d136f680df405c0d3
Emi1Mel0 <emilwagdy11@gmail.com>
2023-10-03 10:21:04 +0300

    add to cart() with routing it

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,8 +1,8 @@
-
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { food } from '../shared/models/food';
 import { FoodService } from '../services/food/food.service';
+import { CartService } from '../services/cart/cart.service';
 
 @Component({
   selector: 'app-details',
@@ -10,17 +10,24 @@ import { FoodService } from '../services/food/food.service';
   styleUrls: ['./details.component.css'],
 })
 export class DetailsComponent implements OnInit {
-  constructor(private _ActivatedRoute:ActivatedRoute, private _FoodService:FoodService) {
-
+  constructor(
+    private _ActivatedRoute: ActivatedRoute,
+    private _FoodService: FoodService,
+    private _CartService: CartService,
+    private _router: Router
+  ) {
     _ActivatedRoute.params.subscribe((params) => {
-      if(params['id'])
-      {
+      if (params['id']) {
         this.food = this._FoodService.getFoodById(params['id']);
       }
-    })
-
+    });
   }
   ngOnInit(): void {}
 
   food!: food;
+
+  addToCart() {
+    this._CartService.addToCart(this.food);
+    this._router.navigateByUrl('cart');
+  }
 }
