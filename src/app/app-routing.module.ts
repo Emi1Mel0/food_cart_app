3a6aece96059b00ff8d2263531474de5ba74d19e
Emi1Mel0 <emilwagdy11@gmail.com>
2023-10-03 05:21:43 +0300

    adding cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'search/:searchTerm', component: HomeComponent },
   { path: 'tag/:tag', component: HomeComponent },
   { path: 'food/:id', component: DetailsComponent },
+  { path: 'cart', component: CartComponent },
 ];
 
 @NgModule({
